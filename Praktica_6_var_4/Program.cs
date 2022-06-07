f14c45575e4a59f5f30be2a508bf991a407c6fc8
Negovora Sasha <90795744+mowek@users.noreply.github.com>
2022-06-07 16:10:32 +0300

    Add files via upload

---
+++ Praktica_6_var_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace OS_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск корневого процессса:");
+            var q = int.Parse(Console.ReadLine());
+            for (int i = 0; i < q; i++)
+            {
+                File.WriteAllText($" Физическая страница №{i}", "");
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                File.WriteAllText($" Виртуальная страница №{i}", "");
+
+            }
+            while (true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var g = new Random().Next(q + 2);
+                    string name = $" Физическая страница №{g}";
+                    if (g < q)
+                    {
+                        for (int k = 0; k < 3000; k++)
+                        {
+                            File.AppendAllText(name, "Заполнитель");
+                        }
+                    }
+                    if (g >= q)
+                    {
+                        for (int k = 0; k < 3000; k++)
+                        {
+                            File.AppendAllText($" Виртуальная страница №{g - q + 1}", "Заполнитель");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
